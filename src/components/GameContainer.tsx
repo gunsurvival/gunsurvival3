90ca7db33068b81a0d57e4a8ca1e2800033f5c31
Đậu Khoa <khoakomlem@gmail.com>
2024-06-05 00:12:40 +0700

    create online world

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,23 +1,43 @@
 import { createWorld } from "@/core/utils/createWorld"
 import { CasualWorld } from "@/core/world/CasualWorld"
 import { genericMemo } from "@/utils/genericMemo"
-import { useMemo } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
+import { Client as ColyseusClient } from "colyseus.js"
+
+async function connect() {
+	const client = new ColyseusClient("ws://localhost:2567")
+	const room = await client.joinOrCreate("casual")
+	console.log("joined room", room.roomId)
+	return { client, room }
+}
+
+async function createClientWorld() {
+	const { room } = await connect()
+	const world = await createWorld(CasualWorld, {
+		mode: "client",
+		room,
+	})
+	return world
+}
 
 export const GameContainer = genericMemo(() => {
 	console.log("GameContainer rendered")
-	const world = useMemo(() => {
-		createWorld(CasualWorld, {
-			mode: "both",
-		}).then(async (w) => {
-			if (w.isServer) {
-				const entity = await w.addEntity("Gunner", {
-					x: 100,
-					y: 100,
-				})
-				const controller = await entity.addController("GunnerController", {})
-				controller.setTarget(entity)
-			}
-		})
+
+	const [world, setWorld] = useState<CasualWorld | undefined>()
+
+	const test = useCallback(async () => {
+		// if (w.isServer) {
+		//   const entity = await w.addEntity("Gunner", {
+		//     x: 100,
+		//     y: 100,
+		//   })
+		//   const controller = await entity.addController("GunnerController", {})
+		//   controller.setTarget(entity)
+		// }
+	}, [])
+
+	useEffect(() => {
+		createClientWorld().then(setWorld)
 	}, [])
 
 	return (
