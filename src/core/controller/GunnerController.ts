337e30bfe86e1d29474f9eebd5d9d0b6112b34c2
Đậu Khoa <khoakomlem@gmail.com>
2024-06-05 21:07:09 +0700

    shooting

--- src/core/controller/GunnerController.ts
+++ src/core/controller/GunnerController.ts
@@ -1,9 +1,10 @@
 import { ServerController } from "@/lib/multiplayer-world/ServerController"
 import { Gunner } from "../entity"
-import { Client, Controller } from "@/lib/multiplayer-world/decorators"
+import { Client, Controller, Server } from "@/lib/multiplayer-world/decorators"
+import EventEmitter from "events"
 
 export class GunnerController extends ServerController<Gunner> {
-	speed = 0.1
+	speed = 0.5
 	state = {
 		up: false,
 		down: false,
@@ -11,12 +12,12 @@ export class GunnerController extends ServerController<Gunner> {
 		right: false,
 		shoot: false,
 	}
+	ee = new EventEmitter()
 
 	setupClient() {
 		console.log("Setting up client")
 		const handleKeydown = (e: KeyboardEvent) => {
 			if (e.key === "w") {
-				console.log("w")
 				this.enable("up")
 			}
 			if (e.key === "s") {
@@ -49,6 +50,14 @@ export class GunnerController extends ServerController<Gunner> {
 				this.disable("shoot")
 			}
 		}
+		const handleClick = (e: MouseEvent) => {
+			this.enable("shoot")
+		}
+		this.ee.addListener("keypress", (key: keyof typeof this.state) => {
+			if (key === "shoot") {
+				this.shoot()
+			}
+		})
 
 		document.addEventListener("keydown", handleKeydown)
 		document.addEventListener("keyup", handleKeyup)
@@ -62,6 +71,7 @@ export class GunnerController extends ServerController<Gunner> {
 	enable(key: keyof typeof this.state) {
 		if (!this.state[key]) {
 			this.setState(key, true)
+			this.ee.emit("keypress", key)
 		}
 	}
 
@@ -73,20 +83,24 @@ export class GunnerController extends ServerController<Gunner> {
 
 	@Controller()
 	setState(key: keyof typeof this.state & string, value: boolean) {
-		if (value) {
-			console.log("Enabling", key)
-		} else {
-			console.log("Disabling", key)
-		}
+		// if (value) {
+		// 	console.log("Enabling", key)
+		// } else {
+		// 	console.log("Disabling", key)
+		// }
 		this.state[key] = value
 	}
 
-	@Controller()
+	@Controller({ serverOnly: true })
 	shoot() {
+		const angle = Math.atan2(this.target.vel.y, this.target.vel.x)
 		// @ts-ignore
 		this.world.addEntity("Bullet", {
-			x: this.target.pos.x,
-			y: this.target.pos.y,
+			x: this.target.pos.x + Math.cos(angle) * 70,
+			y: this.target.pos.y + Math.sin(angle) * 70,
+			velX: Math.cos(angle) * 40,
+			velY: Math.sin(angle) * 40,
+			angle: Math.atan2(this.target.vel.y, this.target.vel.x),
 		})
 	}
 
@@ -104,7 +118,14 @@ export class GunnerController extends ServerController<Gunner> {
 			if (this.state.right) {
 				this.target.acc.x += this.speed
 			}
-			if (this.state.shoot) {
+			this.hookShoot()
+		}
+	}
+
+	@Server()
+	hookShoot() {
+		if (this.state.shoot) {
+			if (this.world.frameCount % 10 === 0) {
 				this.shoot()
 			}
 		}
