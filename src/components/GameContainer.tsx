e78c5b148160c465f57ccd2f41a090c87fe3371b
Đậu Khoa <khoakomlem@gmail.com>
2024-06-05 21:06:58 +0700

    add enable sync checkbox

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -3,9 +3,10 @@ import { CasualWorld } from "@/core/world/CasualWorld"
 import { genericMemo } from "@/utils/genericMemo"
 import { useCallback, useEffect, useMemo, useState } from "react"
 import { Client as ColyseusClient } from "colyseus.js"
+import { Checkbox } from "./ui/checkbox"
 
 async function connect() {
-	const client = new ColyseusClient("ws://localhost:2567")
+	const client = new ColyseusClient("ws://khoakomlem-internal.ddns.net:2567")
 	const room = await client.joinOrCreate("casual")
 	console.log("joined room", room.roomId)
 	return { client, room }
@@ -36,14 +37,28 @@ export const GameContainer = genericMemo(() => {
 		// }
 	}, [])
 
+	const onCheckedChange = useCallback((checked: boolean) => {
+		window.isSync = checked
+	}, [])
+
 	useEffect(() => {
 		createClientWorld().then(setWorld)
+		window.isSync = true
 	}, [])
 
 	return (
-		<div
-			id="game-root"
-			className="fixed top-0 left-0 w-full h-full bg-white"
-		></div>
+		<div className="fixed top-0 left-0 w-full h-full bg-white">
+			<div id="game-root" className="absolute w-full h-full z-0"></div>
+			<div className="absolute z-10">
+				<div className="flex flex-row gap-x-1">
+					<Checkbox
+						defaultChecked
+						onCheckedChange={onCheckedChange}
+						className="z-10 h-6 w-6 stroke-white border-white text-white"
+					/>
+					<p className="text-white">ENABLE SYNC</p>
+				</div>
+			</div>
+		</div>
 	)
 })
