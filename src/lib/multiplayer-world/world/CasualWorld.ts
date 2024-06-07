33211244d8877f3ae77b4d0d89b89d2f69842fc3
Đậu Khoa <khoakomlem@gmail.com>
2024-06-08 05:21:08 +0700

    support entity friction, optimize check collision

--- src/lib/multiplayer-world/world/CasualWorld.ts
+++ src/lib/multiplayer-world/world/CasualWorld.ts
@@ -27,8 +27,8 @@ export abstract class CasualWorld extends World {
 		entity.body?.setPosition(entity.pos.x, entity.pos.y)
 
 		if (entity.vel.len() > 0.01) {
-			entity.vel.x *= 0.91
-			entity.vel.y *= 0.91
+			entity.vel.x *= entity.friction
+			entity.vel.y *= entity.friction
 		}
 		if (entity.acc.x !== 0) {
 			// prevent unnecessary assign setter (for colyseus performance)
@@ -55,35 +55,23 @@ export abstract class CasualWorld extends World {
 
 		// ---- DETECT COLLISIONS ----
 		this.newCollisionHashMap.clear()
-		this.entities.forEach((entity) => {
-			if (!entity.body) return
-			this.physics.updateBody(entity.body)
-
-			this.physics.checkOne(
-				entity.body,
-				({ ...response }: ResponseBodyRefEntity) => {
-					const entityA = response.a.entitiyRef
-					const entityB = response.b.entitiyRef
-
-					if (entityA && entityB) {
-						// if (entityA.isStatic && entityB.isStatic) {
-						// 	// If current both entities are static, skip collision check
-						// 	return
-						// }
-
-						const uniq = `${entityA.id}-${entityB.id}`
-						const serializedResponse = serializeResponse(response)
-						this.newCollisionHashMap.set(uniq, response)
-						if (this.collisionHashMap.has(uniq)) {
-							entity.onCollisionStay(entityB.id, serializedResponse)
-						} else {
-							this.collisionHashMap.set(uniq, response)
-							entityA.onCollisionEnter(entityB.id, serializedResponse)
-						}
-					}
+		this.physics.update()
+		this.physics.checkAll(({ ...response }: ResponseBodyRefEntity) => {
+			const entityA = response.a.entitiyRef
+			const entityB = response.b.entitiyRef
+			if (entityA && entityB) {
+				const uniq = `${entityA.id}-${entityB.id}`
+				const serializedResponse = serializeResponse(response)
+				this.newCollisionHashMap.set(uniq, response)
+				if (this.collisionHashMap.has(uniq)) {
+					entityA.onCollisionStay(entityB.id, serializedResponse)
+				} else {
+					this.collisionHashMap.set(uniq, response)
+					entityA.onCollisionEnter(entityB.id, serializedResponse)
 				}
-			)
+			}
 		})
+
 		this.collisionHashMap.forEach(
 			(response: ResponseBodyRefEntity, uniq: string) => {
 				if (!this.newCollisionHashMap.has(uniq)) {
@@ -102,6 +90,11 @@ export abstract class CasualWorld extends World {
 		this.entities.forEach((entity) => {
 			this.finalizeTick(entity, delta)
 			if (entity.markAsRemoved) {
+				// console.log(
+				// 	"remove entity",
+				// 	entity.constructor.name,
+				// 	this.entities.size
+				// )
 				if (entity.body) this.physics.remove(entity.body)
 				this.entities.delete(entity.id)
 				entity.onRemoveFromWorld()
