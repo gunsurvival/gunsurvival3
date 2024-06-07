aaed283fe9c3a9a15062fb0f673aaf450d567cce
Đậu Khoa <khoakomlem@gmail.com>
2024-06-08 05:18:17 +0700

    bullet trail

--- src/core/entity/Bullet.ts
+++ src/core/entity/Bullet.ts
@@ -1,20 +1,26 @@
-import { Assets, Container, Mesh, MeshRope, Point, Sprite } from "pixi.js"
+import { Assets, Container, Graphics, MeshRope, Point, Texture } from "pixi.js"
 import { Gunner } from "./Gunner"
 import { Circle } from "detect-collisions"
 import { Client, Server } from "@/lib/multiplayer-world/decorators"
 import type { SerializedResponse } from "../../lib/multiplayer-world/utils/dectect-collisions"
 import { PixiEntity } from "@/lib/multiplayer-world/entity/PixiEntity"
 import { Smooth } from "smooth.ts"
+import { Emitter } from "@barvynkoa/particle-emitter"
 
 export class Bullet extends PixiEntity {
-	display: MeshRope | undefined
+	display!: Graphics
+	rope!: MeshRope
 	body = new Circle({ x: 0, y: 0 }, 4)
-	maxTailLength = 10
+	maxTailLength = 20
 	accuracy = 0.5
 	history = new Array<[number, number]>()
 	tails = new Array<Point>()
+	friction = 0.97
 
 	async prepare(options: Parameters<this["init"]>[0]): Promise<void> {
+		this.display = new Graphics()
+		this.display.circle(0, 0, 4).fill("red")
+
 		this.history = Array.from(
 			{ length: this.maxTailLength * this.accuracy },
 			() => [options.pos?.x ?? 0, options.pos?.y ?? 0]
@@ -23,19 +29,25 @@ export class Bullet extends PixiEntity {
 			{ length: this.maxTailLength },
 			() => new Point(options.pos?.x ?? 0, options.pos?.y ?? 0)
 		)
-		this.display = new MeshRope({
-			texture: await Assets.load("images/Bullet2.png"),
+		this.rope = new MeshRope({
+			texture: await Assets.load("images/trail.png"),
 			points: this.tails,
 		})
+		this.rope.tint = "red"
 		this.display.blendMode = "add"
-		// await super.prepare(options)
+		await super.prepare(options)
+		// this.display.addChild(this.rope)
+		this.world.viewport.addChild(this.rope)
+		this.display.on("removed", () => {
+			this.rope.destroy()
+		})
 	}
 
 	nextTick(delta: number) {
 		if (this.vel.len() < 0.1) {
 			this.destroy()
 		}
-		// this.updateDisplay(delta)
+		this.updateDisplay(delta)
 		this.drawTail()
 	}
 
@@ -58,7 +70,6 @@ export class Bullet extends PixiEntity {
 		// @ts-ignore
 		const other = this.world.entities.get(otherId)
 		if (other instanceof Gunner) {
-			other.applyForceByVelocity(this.vel, 0.1)
 			this.destroy()
 		}
 	}
