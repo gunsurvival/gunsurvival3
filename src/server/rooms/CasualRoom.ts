51dc5ac5a63d23c83d5c7989625198d6d644f362
Đậu Khoa <khoakomlem@gmail.com>
2024-06-10 17:01:25 +0700

    new world gen function

--- src/server/rooms/CasualRoom.ts
+++ src/server/rooms/CasualRoom.ts
@@ -1,42 +1,81 @@
 import uniqid from "uniqid"
 import * as Entities from "@/core/entity"
+import * as Items from "@/core/item"
 import { createWorld } from "@/core/utils/createWorld"
 import { Room, Client, type ClientArray } from "@colyseus/core"
 import { UserData } from "../types/UserData"
 import { CasualWorld } from "@/core/world/CasualWorld"
+import WorldmapGenerator from "worldmap-generator"
 
 export class CasualRoom extends Room<CasualWorld> {
 	declare clients: ClientArray<UserData>
 	maxClients = 4
 
+	genWorld() {
+		const wideX = 1920 // Width of the world
+		const wideY = 1080 // Height of the world
+		const objects = ["Bush", "Rock", "Spider", "Wolf"]
+		const numBushes = 10
+		const bushRadius = 5 // Radius of the bush group
+		const numSpiders = 10
+		const numWolves = 5
+
+		// Spawn the bushes and rocks in groups
+		for (let i = 0; i < numBushes; i++) {
+			// Use a normal distribution to get the x and y coordinates for the center of the bush group
+			const bushX = -wideX / 2 + wideX * Math.random()
+			const bushY = -wideY / 2 + wideY * Math.random()
+
+			// Spawn the bush at the center
+			this.state.addEntity("Bush", { pos: { x: bushX, y: bushY } })
+		}
+
+		// Spawn the rocks randomly
+		for (let i = 0; i < numBushes; i++) {
+			const rockX = -wideX / 2 + wideX * Math.random()
+			const rockY = -wideY / 2 + wideY * Math.random()
+			this.state.addEntity("Rock", { pos: { x: rockX, y: rockY } })
+		}
+
+		// Spawn the spiders randomly
+		for (let i = 0; i < numSpiders; i++) {
+			const spiderX = -wideX / 2 + wideX * Math.random()
+			const spiderY = -wideY / 2 + wideY * Math.random()
+			this.state.addEntity("Spider", { pos: { x: spiderX, y: spiderY } })
+		}
+
+		// Spawn the wolves randomly
+		for (let i = 0; i < numWolves; i++) {
+			const wolfX = -wideX / 2 + wideX * Math.random()
+			const wolfY = -wideY / 2 + wideY * Math.random()
+			this.state.addEntity("Wolf", { pos: { x: wolfX, y: wolfY } })
+		}
+	}
+
 	async onCreate(options: any) {
 		const world = createWorld(CasualWorld, {
 			mode: "server",
 			room: this,
-			entityClasses: Entities,
+			// TODO: làm generic cho entityClasses
+			// @ts-ignore
+			entityClasses: { ...Entities, ...Items },
 		})
 		this.setState(world)
-		for (let i = 0; i < 100; i++) {
+		for (let i = 0; i < 1; i++) {
 			world.addEntity("GunnerBot", {
 				x: Math.random() * 500,
 				y: Math.random() * 500,
 			})
 		}
 
-		const wideX = 1920
-		const wideY = 1080
-		const objects = ["Bush", "Rock", "Spider", "Wolf"]
-		for (let i = 0; i < 30; i++) {
-			world.addEntity(objects[Math.floor(Math.random() * objects.length)], {
-				pos: {
-					x: -wideX / 2 + Math.random() * wideX,
-					y: -wideY / 2 + Math.random() * wideY,
-				},
-			})
-		}
+		this.genWorld()
 
 		this.onMessage("ready-to-get-snapshot", async (client, message) => {
 			client.send("snapshot", this.state.getSnapshot())
+		})
+
+		this.onMessage("ping", async (client, message) => {
+			client.send("pong", message)
 		})
 
 		this.onMessage("ready-to-join", async (client, message) => {
